eb79e84f31e8e8408e69f7ca191576dd22261bc1
AlexanderGulik <version1954@gamil.com>
2026-05-31 07:24:51 +0000

    url

--- FrontAdmin/src/config/URLs.ts
+++ FrontAdmin/src/config/URLs.ts
@@ -1,1 +1,2 @@
-export const HOST =  "http://localhost:8080/api";
+//export const HOST =  "http://localhost:8080/api";
+export const HOST =  "https://www.dontploenergo.ru/api";
--- FrontUser/src/config/URL.js
+++ FrontUser/src/config/URL.js
@@ -1,2 +1,2 @@
-//export const HOST = "https://dontploenergo.ru/api";
-export const HOST = "http://localhost:8080/api";
+export const HOST = "https://dontploenergo.ru/api";
+//export const HOST = "http://localhost:8080/api";
